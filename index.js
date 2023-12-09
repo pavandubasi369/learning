f347a7ab694ba3612aa2da2049abf3c4ccd68257
Pavan <pavandubasi369@gmail.com>
2023-12-09 13:19:20 +0530

    added readable code

---
+++ index.js
@@ -0,0 +1,68 @@
+variableOne = 12;
+
+variableTwo = 16;
+
+variableOne>variableTwo;  //false
+
+console.log(variableOne>variableTwo);
+
+variableOne<variableTwo; //true
+
+console.log(variableOne<variableTwo);
+
+var variablebulb = {
+
+    company :"bajaj",
+
+    color : "white",
+
+}
+
+console.log(variablebulb);
+
+var variablecompanyofbulb = "bajaj";
+
+var variablecolor = "white";
+
+
+var variablecompanyofbulb = "bajaj";
+
+console.log("variablecompanyofbulb ,variablecolor");
+
+var companyofbulb = "bajaj";
+
+var color = "white";
+
+
+console.log("companyofbulb ,color");
+
+var companyOfNotebook = "classmate";
+
+var pages = 180;
+
+console.log(companyOfNotebook,pages);
+
+var variableshirt = {
+
+    brandname : 'Denim',
+
+    size : 32,
+
+    color : "black",
+     
+}
+
+console.log(variableshirt);
+
+
+
+var name = "pavan"
+
+var quotationOne = "shift the gear and disappear"
+
+console.log(name);
+
+console.log(quotationOne);
+
+
+
