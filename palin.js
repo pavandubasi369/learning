c61bf0746221130110f058d1b58cb82f846e01b2
Pavan <pavandubasi369@gmail.com>
2023-12-14 18:05:24 +0530

    palindrome.js

---
+++ palin.js
@@ -0,0 +1,18 @@
+// Function to check if a string is a palindrome
+let checkPalindrome = (madam) => {
+
+  return madam === madam.split("").reverse().join("");
+};
+
+console.log(checkPalindrome("madam"));
+
+// Another example
+let checkpal = (pavan) => {
+  
+  return pavan === pavan.split("").reverse().join("");
+};
+
+console.log(checkpal("pavan"));
+
+    
+
