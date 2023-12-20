f59bc1aff7a4399a292d94e21d35fc269cce3938
Pavan <pavandubasi369@gmail.com>
2023-12-20 09:17:52 +0530

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,36 @@
+var numberOne  = 12;
+var numberTwo  = 9;
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo; 
+}
+console.log(" Addition of 12 & 9:",numberOne+numberTwo);
+
+//=========
+
+function multi(numberOne, numberTwo) {
+  return numberOne * numberTwo; 
+}
+console.log("Multiplication of 12 & 9 = :",numberOne*numberTwo);
+
+//=========
+
+function sub(numberOne, numberTwo) {
+  return numberOne - numberTwo; 
+}
+console.log("Subraction of 12 & 9 =:", numberOne-numberTwo);
+
+//=========
+
+function division(numberOne, numberTwo) {
+  return numberOne / numberTwo; 
+}
+console.log("Division of 12 & 9 =:",numberOne/numberTwo);
+
+//=========
+
+function modulus(numberOne, numberTwo) {
+  return numberOne % numberTwo; 
+}
+console.log("Modulus of 12 & 9 =:",numberOne%numberTwo);
+
