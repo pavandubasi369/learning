27d24f2ef9fc7815fe6fff2658c0e03d821a0433
Pavan <pavandubasi369@gmail.com>
2023-12-20 09:18:22 +0530

    calculator.js

---
+++ addm.js
@@ -0,0 +1,28 @@
+var variableOne= 5;
+var variableTwo=4;
+function add(a,b) {
+    return a+b;
+}
+console.log("addition:",add(variableOne,variableTwo));
+
+function sub(a,b) {
+    return a-b;
+}
+console.log("subtraction:",sub(variableOne,variableTwo));
+
+function mutli(a,b) {
+    return a*b;
+}
+console.log("multipication:",mutli(variableOne,variableTwo));
+
+function div(a,b) {
+    return a/b;
+}
+console.log("division:",div(variableOne,variableTwo));
+   
+function mod(a,b) {
+    return a%b;
+}
+console.log("modulo:",add(variableOne,variableTwo));
+   
+    
