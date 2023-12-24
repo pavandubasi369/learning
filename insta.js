7a50f071f4cfbdea39a58e9c82ad3e00166c44d7
Pavan <pavandubasi369@gmail.com>
2023-12-24 16:31:58 +0530

    instagram.js

---
+++ insta.js
@@ -0,0 +1,63 @@
+var instagramProfile = {
+    userName : "__pa__1____",
+    bio : "SUKOON",
+    followers : 269,
+    following : 140,
+    reels : 1,
+    storyHighlights : 2,
+    savedCollection : 26,
+    favourites : 33,
+
+
+
+    posts : [
+        {
+            postOneLikes : 100,
+            postOneComments : 2,
+        },
+        {
+            postTwoLikes : 102,
+            postTwoComments : 0, 
+        },
+        {
+            postThreeLikes : 87,
+            postThreeComments : 0,
+        },
+        {
+            postFourLikes : 80,
+            postFourComments : 1,
+        },
+        {
+            postFiveLikes : 82,
+            postFiveComments : 1,
+        },
+        {
+            postSixLikes : 62,
+            postSixComments : 1,
+        },
+    ],
+    home : [
+        {
+            logo : "INSTAGRAM",
+            notifications : 3,
+            myStory : [
+                {
+                    storyOneViews : 89,
+                    storyOneLikes : 3,
+                },
+                {
+                
+                    storyTwoViews : 86,
+                    storyTwoLikes : 2,
+                },
+            ],
+            friendsStories : 5,
+            closeFriendsStories : 2,
+            chats : 8,
+            newsFeed : "trending content",
+        }
+    ]
+    
+}
+
+console.log(instagramProfile);
