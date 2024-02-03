8b610aaf23a848ceb087c493e34f9eb797419fa9
Pavan <pavandubasi369@gmail.com>
2024-02-03 14:13:27 +0530

    latestupload

--- insta.js
+++ insta.js
@@ -60,4 +60,4 @@ var instagramProfile = {
     
 }
 
-console.log(instagramProfile.home[0].closeFriendsStories);
+console.log(instagramProfile.reels);
---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+create a variable whose value is object of instagram reels
+*/
+
+var instagramReels = {
+    comedyReels : 5,
+    action
+    
+}
